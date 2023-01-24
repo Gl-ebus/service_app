4e2f53f9003d9d8f7cc0e908bce648ab8e719873
Gleb <gleb82@ngs.ru>
2023-01-24 13:26:29 +0700

    3. В вывод списка подписок добавлена цена с учётом скидки, а также общая стоимость всех подписок (total_amount)

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -13,5 +13,17 @@ class SubscriptionView(ReadOnlyModelViewSet):
 		Prefetch('client',
  		queryset=Client.objects.select_related('user').
 		         only('company_name','user__email'))
-	)
+		).annotate(price=F('service__full_price') -
+	           F('service__full_price') * F('plan__discount_percent') / 100.00)
 	serializer_class = SubscriptionSerializer
+
+	def list(self, request, *args, **kwargs):
+		response = super().list(request, *args, **kwargs)
+
+		response_data = {'result': response.data}
+		response_data['total_amount'] = self.queryset.aggregate(
+										total=Sum('price')
+										).get('total')
+		response.data = response_data
+
+		return response
